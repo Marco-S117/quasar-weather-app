c5f6d8d327c9a5ee52abbaa57b0b64cabd776fad
Marco-S117 <polino47@gmail.com>
2020-12-16 19:34:49 +0100

    Rimossa icona GPS dal drawer

--- src/components/partials/DrawerAppSettings.vue
+++ src/components/partials/DrawerAppSettings.vue
@@ -4,25 +4,6 @@
     <q-item-label header>Settings</q-item-label>
     <q-item>
       <q-item-section
-        @click="geoLocation = !geoLocation"
-        top
-        avatar
-        class="pointer"
-      >
-        <q-avatar
-          :color="geoLocation ? 'positive' : 'negative'"
-          :icon="geoLocation ? 'gps_fixed' : 'gps_off'"
-          text-color="white"
-        />
-      </q-item-section>
-      <q-item-section>
-        <q-item-label>Device Position</q-item-label>
-        <q-item-label caption lines="2">Detect your position.</q-item-label>
-      </q-item-section>
-    </q-item>
-    <!-- Color Settings -->
-    <q-item>
-      <q-item-section
         @click="darkTheme = !darkTheme"
         top
         avatar
@@ -50,17 +31,9 @@ export default {
     geoLocation: false,
     darkTheme: true
   }),
-  methods: {
-    toogleGeoLocation () {
-      this.geoLocation = !this.geoLocation
-    }
-  },
   watch: {
     darkTheme: function () {
       Dark.toggle()
-    },
-    geoLocation: function (state) {
-      console.log('Toogle location', state)
     }
   }
 }
