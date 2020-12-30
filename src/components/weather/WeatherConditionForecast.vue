8aa038db9b398e3832454a12bca2ab09d493f57b
Marco-S117 <polino47@gmail.com>
2020-12-30 17:58:08 +0100

    Finita sezione forecast

--- src/components/weather/WeatherConditionForecast.vue
+++ src/components/weather/WeatherConditionForecast.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
-    <p>{{ day }}</p>
+    <div class="text-h5 text-center text-uppercase text-grey q-mb-lg">
+      {{ day.date_epoch | dayName }}
+    </div>
+    <div
+      v-for="(hour, index) in dayHours"
+      :key="index"
+      class="flex items-center justify-between q-my-sm"
+    >
+      <span class="text-primary">{{ hour.time_epoch | dayHour }}</span>
+      <span class="text-secondary">
+        {{ Math.round(hour.temp_c) }} °C
+      </span>
+      <span class="text-info">{{ hour.humidity }}%</span>
+      <ConditionIcon
+        :code="hour.condition.code"
+        :isDay="hour.is_day"
+        :width="35"
+        :height="35"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { date } from 'quasar'
 import ConditionIcon from 'components/partials/ConditionIcon'
 
 export default {
-  name: 'WeatherCondition',
+  name: 'WeatherConditionForecast',
   components: {
     ConditionIcon
   },
@@ -18,12 +38,21 @@ export default {
       required: true
     }
   },
+  computed: {
+    dayHours () {
+      return this.day.hour.filter((h, i) => i % 4 === 0 ? h : '')
+    }
+  },
+  filters: {
+    dayName (timeStamp) {
+      return date.formatDate((timeStamp * 1000), 'dddd')
+    },
+    dayHour (timeStamp) {
+      return date.formatDate((timeStamp * 1000), 'HH:mm')
+    }
+  },
   mounted() {
     console.log('Day forecast', this.day)
   }
 }
 </script>
-
-<style>
-
-</style>
